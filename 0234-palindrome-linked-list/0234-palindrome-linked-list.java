ec7e0361b76cd7e5a3f2c30f69cab9cac501e1d6
DEVA <devakrs07@gmail.com>
2026-03-23 14:27:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -40,14 +40,4 @@ class Solution {
         }
         return true;
     }
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter f = new FileWriter("display_runtime.txt")) {
-                f.write("0");
-            } catch (Exception e) {
-
-            }
-        }));
-}
 }
