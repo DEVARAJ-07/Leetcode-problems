1dce493bd597e7cc1528c6010f189b33734abb31
DEVA <devakrs07@gmail.com>
2026-04-05 19:31:08 +0530

    Time: 26 ms (6.54%), Space: 76.7 MB (87.27%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int flag=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+               flag=1;
+            }
+        }
+        if(flag!=0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
